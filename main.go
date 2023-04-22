6d51e8c50bea9176dc0957c9689446e1bcd4f1bf
halegreen <1141195807@qq.com>
2023-04-22 22:30:45 +0800

    add

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
-	"k8s.io/kubernetes/cmd/kube-scheduler/app"
+	"fmt"
 	"math/rand"
+	"os"
+	"time"
+
+	"k8s.io/component-base/logs"
+	"k8s.io/kubernetes/cmd/kube-scheduler/app"
+
+	simple "github.com/halegreen/simple-custom-scheduler/pkg"
 )
 
 func main() {
-	rand.Seed()
-	command := app.NewSchedulerCommand(app.WithPlugin())
+	rand.Seed(time.Now().UTC().UnixNano())
+	command := app.NewSchedulerCommand(app.WithPlugin(simple.Name, simple.NewSimplePlugin))
+	logs.InitLogs()
+
+	if err := command.Execute(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
 }
--- pkg/plugins.go
+++ pkg/plugins.go
@@ -1,1 +1,55 @@
 package plugins
+
+import (
+	"context"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/kubernetes/pkg/scheduler/framework"
+)
+
+const (
+	Name = "simple"
+)
+
+type SimplePlugin struct {
+}
+
+// NewSimplePlugin initializes and returns a new simple plugin.
+func NewSimplePlugin(obj runtime.Object, f framework.Handle) (framework.Plugin, error) {
+	return &SimplePlugin{}, nil
+}
+
+func (p *SimplePlugin) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
+	return framework.NewStatus(framework.Success)
+}
+
+func (p *SimplePlugin) PreFilter(ctx context.Context, state *framework.CycleState, pod *v1.Pod) *framework.Status {
+	return framework.NewStatus(framework.Success)
+}
+
+func (cs *SimplePlugin) PreFilterExtensions() framework.PreFilterExtensions {
+	return nil
+}
+
+func (p *SimplePlugin) PostFilter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, filteredNodeStatusMap framework.NodeToStatusMap) (*framework.PostFilterResult, *framework.Status) {
+	return nil, framework.NewStatus(framework.Success)
+}
+
+func (p *SimplePlugin) Score(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
+	return 0, framework.NewStatus(framework.Success)
+}
+
+func (p *SimplePlugin) ScoreExtensions() framework.ScoreExtensions {
+	return nil
+}
+
+func (p *SimplePlugin) Reserve(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) *framework.Status {
+	return framework.NewStatus(framework.Success)
+}
+
+func (p *SimplePlugin) Unreserve(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) {
+}
+
+func (p *SimplePlugin) Name() string {
+	return Name
+}
